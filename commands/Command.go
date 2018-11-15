d08f0f11119ec884577a1a92f7dbcbe2e4ca8ee6
liuz <lzol@139.com>
2018-11-15 17:59:09 +0800

    commands

--- commands/Command.go
+++ commands/Command.go
@@ -1,4 +1,9 @@
 package commands
 
+type Command struct {
+	CommandType enums.EnumsCommand
+}
+
 type Commands struct {
+	commands []Command `json:"commands"`
 }
---
+++ enums/EnumsCommand.go
@@ -0,0 +1,19 @@
+package enums
+
+const (
+	INSERT             = "insert"
+	BATCH_EXECUTION    = "batch-execution"
+	RETRACT            = "retract"
+	MODIFY             = "modify"
+	GET_OBJECT         = "get-object"
+	INSERT_ELEMENTS    = "insert-elements"
+	FIREALLRULES       = "fire-all-rules"
+	START_PROCESS      = "start-process"
+	SIGNAL_EVENT       = "signal-event"
+	COMPLETE_WORK_ITEM = "complete-work-item"
+	ABORT_WORK_ITEM    = "abort-work-item"
+	QUERY              = "query"
+	SET_GLOBAL         = "set-global"
+	GET_GLOBAL         = "get-global"
+	GET_OBJECTS        = "get-objects"
+)
